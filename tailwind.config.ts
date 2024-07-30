1054e37cc3312fa9e3eb747f6fd3ccc58471943e
Dev_Faruq <olatoyesefaruq@gmail.com>
2024-07-30 11:47:54 +0100

    delete comfirmation page component

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        custom: '0px 1px 18px 0px rgba(10, 57, 176, 0.12)',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
